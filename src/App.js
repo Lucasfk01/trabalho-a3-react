1102f0f91aa7d712147e0990442b30b51326bf5a
FelipeCasarano <felipe262@outlook.com>
2023-10-22 16:34:10 -0300

    Interface Gráfica

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React, { useState } from 'react';
 
+import Home from './page/components/home';
+
 const App = () => {
-  const [valorInput, setValorInput] = useState('');
 
-  const handleChangeInput = (event) => {
-    setValorInput(event.target.value);
-  };
 
-  
   return (
-    <div
-      style={{
-        backgroundColor: 'lightblue',
-        minHeight: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <div>
-        <label htmlFor="seu-input">Nome: </label>
-        <input
-          style={{
-            width: '500px',
-            padding: '8px',
-          }}
-          type="text"
-          id="seu-input"
-          placeholder="Digite o nome do Livro ou Artigo que deseja procurar."
-          value={valorInput}
-          onChange={handleChangeInput}
-        />
-      </div>
-    </div>
+    <Home />
+
   );
 };
 
