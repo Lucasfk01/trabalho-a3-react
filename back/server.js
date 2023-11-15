96491a9dbfb7315d7a6b0f2f9b76e4177ea07274
Lucas Kimura <lucasfkimura@hotmail.com>
2023-11-15 15:06:09 -0500

    feat: entrega do setup de backend

---
+++ back/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const {OpenAI } = require("openai");
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.post('/api/data', async (req, res) => {
+    const { text } = req.body;
+    try {
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "user",
+                    content: `Faça um resumo do livro/artigo: ${text}`
+                }
+            ],
+            temperature: 0,
+            max_tokens: 1024,
+        });
+        res.json({ resumo: response.choices[0].message.content });
+    } catch (error) {
+        console.error('Erro na API da OpenAI:', error);
+        res.status(500).send('Erro ao processar a solicitação');
+    }
+});
+
+const port = 3001;
+app.listen(port, () => console.log(`Server running on port ${port}`));
